f2f6bcdbeffe2c327d3a340040d39fe62f5d60da
kslart <kslartey@st.ug.edu.gh>
2024-05-27 16:24:10 +0000

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:24}}><Text>My name is <Text style={{fontWeight:"bold"}}>Kofi</Text>
+      <Text style={{fontSize:24}}>My name is <Text style={{fontWeight:"bold"}}>Kofi</Text>
   </Text>
       <StatusBar style="auto" />
     </View>
